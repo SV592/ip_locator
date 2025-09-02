391207741bbe7e5423424e952507d5a454cd95a3
Shaquille Pearson <shaquillepearson9@gmail.com>
2025-09-02 14:52:18 -0400

    globe component

--- src/app/components/Globe.tsx
+++ src/app/components/Globe.tsx
@@ -1,69 +1,36 @@
 "use client";
-import React, { useRef, useMemo } from "react";
+import React, { useRef } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 
+import { Stars } from "./Stars";
+import { Countries } from "./Countries";
+import { States } from "./States";
+
 const GlobeGeometry = () => {
   const groupRef = useRef<THREE.Group>(null);
 
-  useFrame(() => {
-    if (groupRef.current) {
-      groupRef.current.rotation.y += 0.01;
-    }
-  });
+  // useFrame(() => {
+  //   if (groupRef.current) {
+  //     groupRef.current.rotation.y += 0.01;
+  //   }
+  // });
 
   return (
     <group ref={groupRef}>
-      {/* Mesh for the solid sphere */}
-      <mesh>
-        <sphereGeometry args={[2, 32, 32]} />
-        <meshLambertMaterial wireframe color={0x9cdba6} />
-      </mesh>
+      {/* mesh for wireframe edges */}
+      <lineSegments>
+        {/* EdgesGeometry takes a BufferGeometry as an argument */}
+        <edgesGeometry args={[new THREE.SphereGeometry(2)]} />
+        <lineBasicMaterial color={"#44444E"} />
+      </lineSegments>
 
-      {/* Mesh for the wireframe edges */}
-      {/* <lineSegments> */}
-      {/* EdgesGeometry takes a BufferGeometry as an argument */}
-      {/* <edgesGeometry args={[new THREE.SphereGeometry(2)]} /> */}
-      {/* <lineBasicMaterial color={0xffffff} /> */}
-      {/* </lineSegments> */}
+      {/* <mesh>
+        <sphereGeometry args={[2, 64, 64]} />
+        <meshPhongMaterial color={"#3a9bdc"} transparent opacity={0.9} />
+      </mesh> */}
     </group>
-  );
-};
-
-const Stars = () => {
-  const starsRef = useRef<THREE.Points>(null);
-
-  // Generate the star field geometry once
-  const starGeometry = useMemo(() => {
-    const starCount = 5000;
-    const starGeometry = new THREE.BufferGeometry();
-    const positions = [];
-
-    for (let i = 0; i < starCount; i++) {
-      const x = THREE.MathUtils.randFloatSpread(2000); // large spread
-      const y = THREE.MathUtils.randFloatSpread(2000);
-      const z = THREE.MathUtils.randFloatSpread(2000);
-      positions.push(x, y, z);
-    }
-
-    starGeometry.setAttribute(
-      "position",
-      new THREE.Float32BufferAttribute(positions, 3)
-    );
-    return starGeometry;
-  }, []);
-
-  useFrame(() => {
-    if (starsRef.current) {
-      starsRef.current.rotation.y += 0.0005; // slow rotatio
-    }
-  });
-
-  return (
-    <points ref={starsRef} geometry={starGeometry}>
-      <pointsMaterial color="white" size={2} sizeAttenuation={false} />
-    </points>
   );
 };
 
@@ -76,8 +43,10 @@ const Globe: React.FC = () => {
       <directionalLight position={[1, 1, 1]} intensity={10} color={0x9cdba6} />
       <GlobeGeometry />
       <Stars />
+      <Countries />
+      <States />
       <perspectiveCamera fov={75} near={0.1} far={1000} position={[0, 0, 5]} />
-      <color attach="background" args={["#212121"]} />
+      <color attach="background" args={["#000000"]} />
     </Canvas>
   );
 };
